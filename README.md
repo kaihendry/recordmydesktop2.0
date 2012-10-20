e3649c7409ad594e961ecfbe640a2cf325c855fb
Kai Hendry <hendry@webconverger.com>
2012-10-21 01:21:07 +0800

    good base for testing

---
+++ README.md
@@ -0,0 +1,25 @@
+# recordmydesktop 2.0 - r2d2.sh
+
+Usage: r2d2.sh [options] [output filename]
+
+Options:
+
+	-h this help
+	-v verbose
+	-d duration in seconds
+	-u upload host
+
+Example:
+
+	r2d2.sh -d 5 -u r2d2.webconverger.org:/srv/www/r2d2.webconverger.org
+
+# TODO - Great HTML5 video experience
+
+* Improve WebM content
+* Make HTML resize to fill screen for best viewing
+* Ensure it works on IOS6 Safari
+
+# Further links
+
+* <http://webconverger.org/screencasting>
+* Need to capture regions in your screencas? See: <https://github.com/lolilolicon/FFcast2>
--- r2d2.sh
+++ r2d2.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+
+usage() {
+cat $(readlink -f $(dirname $0))/README.md
+}
+
+verbose="-loglevel quiet"
+while getopts "o:d:vhu:" o
+do
+	case "$o" in
+	(u) dest="$OPTARG";;
+	(d) duration="-t $OPTARG";;
+	(v) verbose="";;
+	(\?) echo "Invalid option: -$OPTARG" >&2 ;;
+	(h) usage; exit;;
+	(*) break;;
+	esac
+done
+shift $((OPTIND - 1))
 out=${1:-out.webm}
+
 res=$(xdpyinfo | grep 'dimensions:'|awk '{print $2}')
-echo Capturing to $out ^C to stop
-ffmpeg -loglevel quiet -f x11grab -s $res -i :0.0 -vcodec huffyuv -sameq -acodec pcm_s16le -f alsa -i hw:0,0 $out
+echo Capturing $res to $out. ^C to stop.
+ffmpeg $verbose $duration -f x11grab -s $res -i :0.0 -vcodec huffyuv -sameq -acodec pcm_s16le -f alsa -i hw:0,0 $out
+test -s $out || ( rm $out; echo ffmpeg failed, try $0 -v; exit 1 )
 echo "<video controls src=$out></video>" > $out.html
+if test "$dest"
+then
+	today=$(date --iso-8601=date)
+	rsync --progress --remove-source-files $out $out.html $dest/$today/
+	echo -e "\n\n\tSHARE: http://$(basename $dest)/$today/$out.html\n"
+fi
