7415ba5302f2fb292257da6d09ee382bb564ea07
Kai Hendry <hendry@webconverger.com>
2012-10-21 14:55:37 +0800

    webm creation

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ Options:
 	-v verbose
 	-d duration in seconds
 	-u upload host
-	-c specify code, e.g. ffvhuff
 
 Example:
 
@@ -16,11 +15,12 @@ Example:
 
 # TODO - Great HTML5 video experience
 
-* Improve WebM content
+* Improve WebM content, good colours, duration is intact
 * Make HTML resize to fill screen for best viewing
-* Ensure it works on IOS6 Safari
+* Ensure it works on IOS6 Safari, i.e. [H264](http://en.wikipedia.org/wiki/H.264/MPEG-4_AVC) version
 
 # Further links
 
 * <http://webconverger.org/screencasting>
-* Need to capture regions in your screencas? See: <https://github.com/lolilolicon/FFcast2>
+* Need to capture regions in your screencast? See: <https://github.com/lolilolicon/FFcast2>
+* Thanks to JEEB & [klaxa](https://gist.github.com/7dcccbd86fdcce3c4ced)
--- r2d2.sh
+++ r2d2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 usage() {
 cat $(readlink -f $(dirname $0))/README.md
@@ -10,7 +10,6 @@ while getopts "o:d:vhu:c:" o
 do
 	case "$o" in
 	(u) dest="$OPTARG";;
-	(c) codec="$OPTARG";;
 	(d) duration="-t $OPTARG";;
 	(v) verbose="";;
 	(\?) echo "Invalid option: -$OPTARG" >&2 ;;
@@ -19,16 +18,28 @@ do
 	esac
 done
 shift $((OPTIND - 1))
-out=${1:-out.webm}
+today=$(date --iso-8601=date)
+mkdir $today || true
+out=$today/${1:-out.webm}
+if test "${out##*.}" != "webm"; then out=$out.webm; fi
+
+temp=$(mktemp -u "/tmp/$0.XXXX.mkv")
+temp2=$(mktemp  -u "/tmp/$0.XXXX.mkv")
+echo Temp files: $temp $temp2
+trap "rm -f $temp $temp2; exit" EXIT
 
 res=$(xdpyinfo | grep 'dimensions:'|awk '{print $2}')
-echo Capturing $res to $out. ^C to stop.
-ffmpeg -report $duration -f x11grab -s $res -i :0.0 -vcodec $codec -sameq -acodec pcm_s16le -f alsa -i hw:0,0 $out
-test -s $out || ( rm $out; echo ffmpeg failed, try $0 -v; exit 1 )
+echo $0: Capturing $res to $out.
+
+ffmpeg $duration -f x11grab -s $res -r 30 -i :0.0 -f alsa -i hw:0,0 -acodec flac -vcodec ffvhuff $temp
+ffmpeg -i $temp -acodec libvorbis $temp2
+ffmpeg -i $temp2 -acodec copy -vcodec libvpx $out
+
+# Generate HTML source
 echo "<video controls src=$out></video>" > $out.html
+
 if test "$dest"
 then
-	today=$(date --iso-8601=date)
-	rsync --progress --remove-source-files $out *.log $out.html $dest/$today/
-	echo -e "\n\n\tSHARE: http://$(basename $dest)/$today/$out.html\n"
+	rsync -r --progress --remove-source-files $today $dest
+	echo -e "\n\n\tSHARE: http://$(basename $dest)/$out.html\n"
 fi
