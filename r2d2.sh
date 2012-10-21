43419c3b46e4f53aa46bcefa86f3d6c33f4742da
Kai Hendry <hendry@webconverger.com>
2012-10-21 15:12:19 +0800

    logging 3 steps to a single file shame that ffmpeg uses stderr and an insane -report mechanism http://ffmpeg.org/trac/ffmpeg/ticket/1823

--- r2d2.sh
+++ r2d2.sh
@@ -19,21 +19,21 @@ do
 done
 shift $((OPTIND - 1))
 today=$(date --iso-8601=date)
-mkdir $today || true
+mkdir $today &>/dev/null || true
 out=$today/${1:-out.webm}
 if test "${out##*.}" != "webm"; then out=$out.webm; fi
+log=${out%.*}.log
 
 temp=$(mktemp -u "/tmp/$0.XXXX.mkv")
 temp2=$(mktemp  -u "/tmp/$0.XXXX.mkv")
-echo Temp files: $temp $temp2
-trap "rm -f $temp $temp2; exit" EXIT
+trap "rm -f $today $temp $temp2 2>/dev/null; exit" EXIT
 
 res=$(xdpyinfo | grep 'dimensions:'|awk '{print $2}')
 echo $0: Capturing $res to $out.
 
-ffmpeg $duration -f x11grab -s $res -r 30 -i :0.0 -f alsa -i hw:0,0 -acodec flac -vcodec ffvhuff $temp
-ffmpeg -i $temp -acodec libvorbis $temp2
-ffmpeg -i $temp2 -acodec copy -vcodec libvpx $out
+ffmpeg $duration -f x11grab -s $res -r 30 -i :0.0 -f alsa -i hw:0,0 -acodec flac -vcodec ffvhuff $temp 2>&1 | tee $log
+ffmpeg -i $temp -acodec libvorbis $temp2 2>&1 | tee -a $log
+ffmpeg -i $temp2 -acodec copy -vcodec libvpx $out 2>&1 | tee -a $log
 
 # Generate HTML source
 echo "<video controls src=$out></video>" > $out.html
