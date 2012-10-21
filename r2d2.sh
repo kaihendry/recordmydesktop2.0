d28ce08b55d5fe0630a81a06d41b25dd949259b2
Kai Hendry <hendry@webconverger.com>
2012-10-21 16:16:04 +0800

    prettify

--- r2d2.sh
+++ r2d2.sh
@@ -27,19 +27,24 @@ temp2=$(mktemp  -u "/tmp/$0.XXXX.mkv")
 trap "rm -f $today $temp $temp2 2>/dev/null; exit" EXIT
 
 res=$(xdpyinfo | grep 'dimensions:'|awk '{print $2}')
-echo $0: Capturing $res to $out.
+echo -e "\033[1;34m$0\033[m capturing $res to $out."
 test "$duration" || echo Type q then enter to end your screencast
 
 ( set -x
 ffmpeg $duration -f x11grab -s $res -r 30 -i :0.0 -f alsa -i hw:0,0 -acodec flac -vcodec ffvhuff $temp
 ffmpeg -i $temp -acodec libvorbis $temp2
-ffmpeg -i $temp2 -acodec copy -vcodec libvpx $out) 2>&1 | tee $log
+ffmpeg -i $temp2 -b:v 2000k -acodec copy -vcodec libvpx $out) 2>&1 | tee $log
 
 # Generate HTML source
 html=${out%.*}.html
 echo "<video width=640 height=360 controls autoplay src=$(basename $out)></video><pre>" > $html
+
+eval $(ffprobe -v 0 -of flat=s=_ -show_format $out) # trick to video info into shell
+
 ffprobe -v warning -show_format $out >> $html
 echo "</pre><p><a href=$(basename $log)>ffmpeg logfile</a></p><p><a href=https://github.com/kaihendry/recordmydesktop2.0/blob/master/r2d2.sh>r2d2.sh source</a></p>" >> $html
+
+echo -e  "\033[1;34m$0\033[m captured $out, duration: $format_duration seconds"
 
 if test "$dest"
 then
